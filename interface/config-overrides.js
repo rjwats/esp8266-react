2cdfe5fe0dfb2ebd32348949efceed2697694e88
rjwats@gmail.com <rjwats@gmail.com>
2018-03-27 00:08:44 +0100

    rename css file in build

--- interface/config-overrides.js
+++ interface/config-overrides.js
@@ -1,6 +1,7 @@
 const CompressionPlugin = require("compression-webpack-plugin");
 const ManifestPlugin = require('webpack-manifest-plugin');
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const path = require('path');
 const fs = require('fs');
@@ -9,13 +10,16 @@ module.exports = function override(config, env) {
   if (env === "production") {
     // rename the ouput file, we need it's path to be short, for SPIFFS
     config.output.filename = 'js/[name].[chunkhash:4].js';
-    
+
     // disable sourcemap for production build
     config.devtool = false;
 
     // take out the manifest and service worker
     config.plugins = config.plugins.filter(plugin => !(plugin instanceof ManifestPlugin));
     config.plugins = config.plugins.filter(plugin => !(plugin instanceof SWPrecacheWebpackPlugin));
+
+    const extractTextPlugin = config.plugins.find((plugin) => plugin instanceof ExtractTextPlugin);
+    extractTextPlugin.filename = "css/[name].[contenthash:4].css";
 
     // add compression plugin, compress javascript, html and css
     config.plugins.push(new CompressionPlugin({
