4c282ea0392eca9b6fe7b48a4c4ccdbe2a852142
rjwats@gmail.com <rjwats@gmail.com>
2018-04-01 10:35:40 +0100

    modify interface to use new prefixed paths

--- interface/src/constants/Endpoints.js
+++ interface/src/constants/Endpoints.js
@@ -1,38 +1,25 @@
-export const ENDPOINT_ROOT = "http://192.168.0.20";
-export const WS_ROOT = "ws://192.168.0.20";
+const ENDPOINT_HOST = "http://192.168.0.20";
+const ENDPOINT_PATH = "/rest";
+const ENDPOINT_PREFIX = ENDPOINT_HOST + ENDPOINT_PATH;
 
-export const NTP_STATUS_PATH = "/ntpStatus";
-export const NTP_STATUS_ENDPOINT = ENDPOINT_ROOT + NTP_STATUS_PATH;
+export const NTP_STATUS_ENDPOINT = ENDPOINT_PREFIX + "/ntpStatus";
+export const NTP_SETTINGS_ENDPOINT = ENDPOINT_PREFIX + "/ntpSettings";
+export const AP_SETTINGS_ENDPOINT = ENDPOINT_PREFIX + "/apSettings";
+export const AP_STATUS_ENDPOINT = ENDPOINT_PREFIX + "/apStatus";
+export const SCAN_NETWORKS_ENDPOINT = ENDPOINT_PREFIX + "/scanNetworks";
+export const LIST_NETWORKS_ENDPOINT = ENDPOINT_PREFIX + "/listNetworks";
+export const WIFI_SETTINGS_ENDPOINT = ENDPOINT_PREFIX + "/wifiSettings";
+export const WIFI_STATUS_ENDPOINT = ENDPOINT_PREFIX + "/wifiStatus";
+export const OTA_SETTINGS_ENDPOINT = ENDPOINT_PREFIX + "/otaSettings";
 
-export const NTP_SETTINGS_PATH = "/ntpSettings";
-export const NTP_SETTINGS_ENDPOINT = ENDPOINT_ROOT + NTP_SETTINGS_PATH;
+const WEB_SOCKET_HOST = "ws://192.168.0.20";
+const WEB_SOCKET_PATH = "/ws";
+const WEB_SOCKET_PREFIX = calculateWebSocketPrefix(WEB_SOCKET_PATH);
 
-export const AP_SETTINGS_PATH = "/apSettings";
-export const AP_SETTINGS_ENDPOINT = ENDPOINT_ROOT + AP_SETTINGS_PATH;
+export const LIGHT_STRIP_WEBSOCKET_URI = WEB_SOCKET_PREFIX + "/lightStrip";
 
-export const AP_STATUS_PATH = "/apStatus";
-export const AP_STATUS_ENDPOINT = ENDPOINT_ROOT + AP_STATUS_PATH;
-
-export const SCAN_NETWORKS_PATH = "/scanNetworks";
-export const SCAN_NETWORKS_ENDPOINT = ENDPOINT_ROOT + SCAN_NETWORKS_PATH;
-
-export const LIST_NETWORKS_PATH = "/listNetworks";
-export const LIST_NETWORKS_ENDPOINT = ENDPOINT_ROOT + LIST_NETWORKS_PATH;
-
-export const WIFI_SETTINGS_PATH = "/wifiSettings";
-export const WIFI_SETTINGS_ENDPOINT = ENDPOINT_ROOT + WIFI_SETTINGS_PATH;
-
-export const WIFI_STATUS_PATH = "/wifiStatus";
-export const WIFI_STATUS_ENDPOINT = ENDPOINT_ROOT + WIFI_STATUS_PATH;
-
-export const OTA_SETTINGS_PATH = "/otaSettings";
-export const OTA_SETTINGS_ENDPOINT = ENDPOINT_ROOT + OTA_SETTINGS_PATH;
-
-export const LIGHT_STRIP_WEBSOCKET_PATH = "/lightStripWS";
-export const LIGHT_STRIP_WEBSOCKET_URI = calculateWebSocketPath(LIGHT_STRIP_WEBSOCKET_PATH);
-
-function calculateWebSocketPath(webSocketPath) {
-  if (WS_ROOT.length === 0){
+function calculateWebSocketPrefix(webSocketPath) {
+  if (WEB_SOCKET_HOST.length === 0){
     var loc = window.location, webSocketURI;
     if (loc.protocol === "https:") {
         webSocketURI = "wss:";
@@ -41,5 +28,5 @@ function calculateWebSocketPath(webSocketPath) {
     }
     return webSocketURI + "//" + loc.host + webSocketPath;
   }
-  return WS_ROOT + webSocketPath;
+  return WEB_SOCKET_HOST + webSocketPath;
 }
