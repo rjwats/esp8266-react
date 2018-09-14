d2847be8a1ab07611399a2a9043fa061e78d9714
Rick Watson <rjwats@gmail.com>
2018-09-14 15:40:46 +0100

    use .env to configure endpoint root automatically for production and development

--- interface/src/constants/Endpoints.js
+++ interface/src/constants/Endpoints.js
@@ -1,13 +1,11 @@
-const ENDPOINT_HOST = "http://192.168.0.20";
-const ENDPOINT_PATH = "/rest";
-const ENDPOINT_PREFIX = ENDPOINT_HOST + ENDPOINT_PATH;
+const ENDPOINT_ROOT = process.env.REACT_APP_ENDPOINT_ROOT;
 
-export const NTP_STATUS_ENDPOINT = ENDPOINT_PREFIX + "/ntpStatus";
-export const NTP_SETTINGS_ENDPOINT = ENDPOINT_PREFIX + "/ntpSettings";
-export const AP_SETTINGS_ENDPOINT = ENDPOINT_PREFIX + "/apSettings";
-export const AP_STATUS_ENDPOINT = ENDPOINT_PREFIX + "/apStatus";
-export const SCAN_NETWORKS_ENDPOINT = ENDPOINT_PREFIX + "/scanNetworks";
-export const LIST_NETWORKS_ENDPOINT = ENDPOINT_PREFIX + "/listNetworks";
-export const WIFI_SETTINGS_ENDPOINT = ENDPOINT_PREFIX + "/wifiSettings";
-export const WIFI_STATUS_ENDPOINT = ENDPOINT_PREFIX + "/wifiStatus";
-export const OTA_SETTINGS_ENDPOINT = ENDPOINT_PREFIX + "/otaSettings";
+export const NTP_STATUS_ENDPOINT = ENDPOINT_ROOT + "ntpStatus";
+export const NTP_SETTINGS_ENDPOINT = ENDPOINT_ROOT + "ntpSettings";
+export const AP_SETTINGS_ENDPOINT = ENDPOINT_ROOT + "apSettings";
+export const AP_STATUS_ENDPOINT = ENDPOINT_ROOT + "apStatus";
+export const SCAN_NETWORKS_ENDPOINT = ENDPOINT_ROOT + "scanNetworks";
+export const LIST_NETWORKS_ENDPOINT = ENDPOINT_ROOT + "listNetworks";
+export const WIFI_SETTINGS_ENDPOINT = ENDPOINT_ROOT + "wifiSettings";
+export const WIFI_STATUS_ENDPOINT = ENDPOINT_ROOT + "wifiStatus";
+export const OTA_SETTINGS_ENDPOINT = ENDPOINT_ROOT + "otaSettings";
