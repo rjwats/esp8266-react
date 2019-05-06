a4220d631d1109f1b658ef2a9326b1a46f48ae7b
Rick Watson <rjwats@gmail.com>
2019-05-06 22:40:24 +0100

    Remove un-necessarry HMAC implementation, use SDK provided implementation

--- src/ArduinoJsonJWT.cpp
+++ src/ArduinoJsonJWT.cpp
@@ -6,13 +6,35 @@ void ArduinoJsonJWT::setSecret(String secret){
     _secret = secret;
 }
 
-String ArduinoJsonJWT::sign(String &value) {
-  // create signature
-  Sha256.initHmac((uint8_t*) _secret.c_str(), _secret.length());
-  Sha256.print(value);
-
-  // trim and return
-  return encode(Sha256.resultHmac(), 32);
+/*
+ * ESP32 uses mbedtls, ESP2866 uses bearssl.
+ * 
+ * Both come with decent HMAC implmentations supporting sha256, as well as others.
+ * 
+ * No need to pull in additional crypto libraries - lets use what we already have.
+ */
+String ArduinoJsonJWT::sign(String &payload) {
+  unsigned char hmacResult[32];
+  {
+  #if defined(ESP_PLATFORM)
+    mbedtls_md_context_t ctx;
+    mbedtls_md_type_t md_type = MBEDTLS_MD_SHA256;  
+    mbedtls_md_init(&ctx);
+    mbedtls_md_setup(&ctx, mbedtls_md_info_from_type(md_type), 1);
+    mbedtls_md_hmac_starts(&ctx, (unsigned char *) _secret.c_str(), _secret.length());
+    mbedtls_md_hmac_update(&ctx, (unsigned char *) payload.c_str(), payload.length());
+    mbedtls_md_hmac_finish(&ctx, hmacResult);
+    mbedtls_md_free(&ctx);
+  #else
+    br_hmac_key_context keyCtx;
+    br_hmac_key_init(&keyCtx, &br_sha256_vtable, _secret.c_str(), _secret.length());
+    br_hmac_context hmacCtx;
+    br_hmac_init(&hmacCtx, &keyCtx, 0);
+    br_hmac_update(&hmacCtx, payload.c_str(), payload.length());
+    br_hmac_out(&hmacCtx, hmacResult);
+  #endif
+  }
+  return encode(hmacResult, 32);
 }
 
 String ArduinoJsonJWT::decode(unsigned char * value) {
@@ -52,7 +74,7 @@ String ArduinoJsonJWT::buildJWT(JsonObject &payload) {
 
   // add the header to payload
   jwt = JWT_HEADER + '.' + jwt;
- 
+  
   // add signature
   jwt  += '.' + sign(jwt);
 
--- src/ArduinoJsonJWT.h
+++ src/ArduinoJsonJWT.h
@@ -1,11 +1,16 @@
 #ifndef ArduinoJsonJWT_H
 #define ArduinoJsonJWT_H
 
-#include "sha256.h"
 #include "base64.h"
 
 #include <Arduino.h>
 #include <ArduinoJson.h>
+
+#if defined(ESP_PLATFORM)
+  #include <mbedtls/md.h>
+#else
+  #include <bearssl/bearssl_hmac.h>
+#endif 
 
 #define JWT_HEADER_SIZE 36
 #define JWT_SIG_SIZE 43
