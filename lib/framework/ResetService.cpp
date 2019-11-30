1ded736b9a13ba5f176dea5290248fb5f821304c
Rick Watson <rjwats@gmail.com>
2019-11-30 13:15:59 +0000

    perfer restart on ESP32 platform

--- lib/framework/ResetService.cpp
+++ lib/framework/ResetService.cpp
@@ -1,14 +1,18 @@
 #include <ResetService.h>
 
 ResetService::ResetService(AsyncWebServer* server, SecurityManager* securityManager) {
-  server->on(RESET_SERVICE_PATH, HTTP_POST,
-    securityManager->wrapRequest(std::bind(&ResetService::reset, this, std::placeholders::_1), AuthenticationPredicates::IS_ADMIN)
-  );
+  server->on(RESET_SERVICE_PATH, HTTP_POST, securityManager->wrapRequest(
+    std::bind(&ResetService::reset, this, std::placeholders::_1), AuthenticationPredicates::IS_ADMIN
+  ));
 }
 
-void ResetService::reset(AsyncWebServerRequest *request) {  
-  request->onDisconnect([](){
+void ResetService::reset(AsyncWebServerRequest* request) {
+  request->onDisconnect([]() {
+#if defined(ESP8266)
     ESP.reset();
+#elif defined(ESP_PLATFORM)
+    ESP.restart();
+#endif
   });
   request->send(200);
 }
