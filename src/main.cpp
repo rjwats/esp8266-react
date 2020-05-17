ba969f05c7fc0460877bbb1f48a2f6a7fa28b966
Rick Watson <rjwats@gmail.com>
2020-05-17 14:21:56 +0100

    use precompiler to choose correct FS implmementation at build time

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,21 @@
 #include <ESP8266React.h>
 #include <LightMqttSettingsService.h>
 #include <LightStateService.h>
+
+#ifdef ESP32
 #include <FS.h>
+#define ESPFS SPIFFS
+#elif defined(ESP8266)
+#include <LittleFS.h>
+#define ESPFS LittleFS
+#endif
 
 #define SERIAL_BAUD_RATE 115200
 
 AsyncWebServer server(80);
-ESP8266React esp8266React(&server, &SPIFFS);
+ESP8266React esp8266React(&server, &ESPFS);
 LightMqttSettingsService lightMqttSettingsService =
-    LightMqttSettingsService(&server, &SPIFFS, esp8266React.getSecurityManager());
+    LightMqttSettingsService(&server, &ESPFS, esp8266React.getSecurityManager());
 LightStateService lightStateService = LightStateService(&server,
                                                         esp8266React.getSecurityManager(),
                                                         esp8266React.getMqttClient(),
@@ -20,9 +27,9 @@ void setup() {
 
   // start the file system (must be done before starting the framework)
 #ifdef ESP32
-  SPIFFS.begin(true);
+  ESPFS.begin(true);
 #elif defined(ESP8266)
-  SPIFFS.begin();
+  ESPFS.begin();
 #endif
 
   // start the framework and demo project
