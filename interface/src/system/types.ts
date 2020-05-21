ade3d7eb0e5dac80c970417113a52b72b57f4174
kasedy <kasedy@gmail.com>
2020-05-20 22:29:42 -0700

    Added Sdk Version and Heap fragmentation to System page

--- interface/src/system/types.ts
+++ interface/src/system/types.ts
@@ -1,10 +1,12 @@
 export interface SystemStatus {
   esp_platform: string;
+  sdk_version: string;
   cpu_freq_mhz: number;
   free_heap: number;
   sketch_size: number;
   free_sketch_space: number;
   flash_chip_size: number;
+  heap_fragmentation: number;
 }
 
 export interface OTASettings {
