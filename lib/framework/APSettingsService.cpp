ef4d6a73be185ad852554b326231bc36fe68d0ce
Rick Watson <rjwats@gmail.com>
2020-07-07 21:13:09 +0100

    allow ap ip address to be configured via ui

--- lib/framework/APSettingsService.cpp
+++ lib/framework/APSettingsService.cpp
@@ -45,6 +45,7 @@ void APSettingsService::manageAP() {
 
 void APSettingsService::startAP() {
   Serial.println(F("Starting software access point"));
+  WiFi.softAPConfig(_state.localIP, _state.gatewayIP, _state.subnetMask);
   WiFi.softAP(_state.ssid.c_str(), _state.password.c_str());
   if (!_dnsServer) {
     IPAddress apIp = WiFi.softAPIP();
