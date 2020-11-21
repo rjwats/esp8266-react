2ba9bab5e050b574006103a53cda0629ee3ae7ef
Rick Watson <rjwats@gmail.com>
2020-11-21 20:22:17 +0000

    add load and save buttons

--- interface/src/project/AudioLightSettingsForm.tsx
+++ interface/src/project/AudioLightSettingsForm.tsx
@@ -1,18 +1,52 @@
 import React, { Fragment } from 'react';
 import { TextField, MenuItem } from '@material-ui/core';
+import SaveIcon from '@material-ui/icons/Save';
+import LoadIcon from '@material-ui/icons/SaveAlt';
 
 import AudioLightLightningMode from './modes/AudioLightLightningMode';
 import AudioLightRainbowMode from './modes/AudioLightRainbowMode';
 import AudioLightFireMode from './modes/AudioLightFireMode';
 import AudioLightColorMode from './modes/AudioLightColorMode';
 
-import { WebSocketFormProps } from '../components';
+import { FormActions, FormButton, WebSocketFormProps } from '../components';
 import { AudioLightModeType, AudioLightMode } from './types';
 import AudioLightConfettiMode from './modes/AudioLightConfettiMode';
+import { redirectingAuthorizedFetch } from '../authentication';
+import { AUDIO_LIGHT_LOAD_SETTINGS_ENDPOINT, AUDIO_LIGHT_SAVE_SETTINGS_ENDPOINT } from './AudioLightSettingsController';
 
 type AudioLightSettingsFormProps = WebSocketFormProps<Partial<AudioLightMode>>;
 
 class AudioLightSettingsForm extends React.Component<AudioLightSettingsFormProps> {
+
+  saveMode = async () => {
+    const { enqueueSnackbar } = this.props;
+    try {
+      const response = await redirectingAuthorizedFetch(AUDIO_LIGHT_SAVE_SETTINGS_ENDPOINT, { method: 'POST' });
+      if (response.status === 200) {
+        enqueueSnackbar("Mode settings saved", { variant: 'success' });
+      } else {
+        throw new Error(`Unexpected response code ${response.status}`);
+      }
+    } catch (error) {
+      const errorMessage = error.message || "Unknown error";
+      enqueueSnackbar("Problem saving: " + errorMessage, { variant: 'error' });
+    }
+  }
+
+  loadMode = async () => {
+    const { enqueueSnackbar } = this.props;
+    try {
+      const response = await redirectingAuthorizedFetch(AUDIO_LIGHT_LOAD_SETTINGS_ENDPOINT, { method: 'POST' });
+      if (response.status === 200) {
+        enqueueSnackbar("Mode settings loaded", { variant: 'success' });
+      } else {
+        throw new Error(`Unexpected response code ${response.status}`);
+      }
+    } catch (error) {
+      const errorMessage = error.message || "Unknown error";
+      enqueueSnackbar("Problem loading: " + errorMessage, { variant: 'error' });
+    }
+  }
 
   selectModeComponent(): React.ElementType | null {
     const mode_id = this.props.data.mode_id;
@@ -55,6 +89,14 @@ class AudioLightSettingsForm extends React.Component<AudioLightSettingsFormProps
           <MenuItem value={AudioLightModeType.PRIDE}>Pride</MenuItem>
         </TextField>
         { ModeComponent && <ModeComponent {...this.props} />}
+        <FormActions>
+          <FormButton startIcon={<SaveIcon />} variant="contained" color="primary" onClick={this.saveMode}>
+            Save Settings
+          </FormButton>
+          <FormButton startIcon={<LoadIcon />} variant="contained" color="secondary" onClick={this.loadMode}>
+            Load Settings
+          </FormButton>
+        </FormActions>
       </Fragment>
     );
   }
