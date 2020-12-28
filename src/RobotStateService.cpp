d25e0b4677d382aa0528a13fb7ef0949d041bb24
Rick Watson <rjwats@gmail.com>
2020-12-28 15:44:06 +0000

    add motor control

--- src/RobotStateService.cpp
+++ src/RobotStateService.cpp
@@ -19,14 +19,26 @@ RobotStateService::RobotStateService(AsyncWebServer* server, SecurityManager* se
 }
 
 void RobotStateService::begin() {
-  camXServo.attach(CAM_X_SERVO_PIN, 2);
-  camYServo.attach(CAM_Y_SERVO_PIN, 3);
-  clawServo.attach(CLAW_SERVO_PIN, 4);
+  camXServo.attach(CAM_X_SERVO_PIN, CAM_X_SERVO_CHANNEL);
+  camYServo.attach(CAM_Y_SERVO_PIN, CAM_Y_SERVO_CHANNEL);
+  clawServo.attach(CLAW_SERVO_PIN, CLAW_SERVO_CHANNEL);
 
   // configure the default state
   _state.camX = DEFAULT_CAM_X;
   _state.camY = DEFAULT_CAM_Y;
   _state.claw = DEFAULT_CLAW;
+  _state.driveX = DEFAULT_DRIVE_X;
+  _state.driveY = DEFAULT_DRIVE_Y;
+
+  // configure the PWM pins for both motors
+  ledcSetup(LEFT_MOTOR_PWM_CHANNEL, MOTOR_PWM_FREQUENCY, MOTOR_PWM_RESOLUTION);
+  ledcAttachPin(LEFT_MOTOR_PWM_PIN, LEFT_MOTOR_PWM_CHANNEL);
+  ledcSetup(RIGHT_MOTOR_PWM_CHANNEL, MOTOR_PWM_FREQUENCY, MOTOR_PWM_RESOLUTION);
+  ledcAttachPin(RIGHT_MOTOR_PWM_PIN, RIGHT_MOTOR_PWM_CHANNEL);
+
+  // configure direction pins for both motors
+  pinMode(LEFT_MOTOR_DIRECTION_PIN, OUTPUT);
+  pinMode(RIGHT_MOTOR_DIRECTION_PIN, OUTPUT);
 
   // apply the default state
   onConfigUpdated();
@@ -36,4 +48,77 @@ void RobotStateService::onConfigUpdated() {
   camXServo.write(_state.camX);
   camYServo.write(_state.camY);
   clawServo.write(_state.claw);
+
+  // calculate and convert motor outputs to PWM outputs for h-bridge
+  struct MotorOutputs outputs = calculateMotorOutputs(_state.driveX, _state.driveY);
+  applyMotorOutput(outputs.left, LEFT_MOTOR_DIRECTION_PIN, LEFT_MOTOR_PWM_CHANNEL);
+  applyMotorOutput(outputs.right, RIGHT_MOTOR_DIRECTION_PIN, RIGHT_MOTOR_PWM_CHANNEL);
+}
+
+void RobotStateService::applyMotorOutput(int32_t output, uint8_t directionPin, uint8_t pwmChan) {
+  bool forward = output >= 0;
+  output = abs(output);
+  output = output > DEAD_ZONE ? output : 0;
+  if (output > 0) {
+    output = map(output, 0, 1023, 600, 1023);
+  }
+  if (forward) {
+    digitalWrite(directionPin, HIGH);
+    ledcWrite(pwmChan, 1023 - output);
+  } else {
+    digitalWrite(directionPin, LOW);
+    ledcWrite(pwmChan, output);
+  }
+}
+
+// Wonderful code below borrowed from https://www.impulseadventure.com/elec/robot-differential-steering.html
+struct MotorOutputs RobotStateService::calculateMotorOutputs(int32_t nJoyX, int32_t nJoyY) {
+  // OUTPUTS
+  int32_t nMotMixL;  // Motor (left)  mixed output           (-1023..+1023)
+  int32_t nMotMixR;  // Motor (right) mixed output           (-1023..+1023)
+
+  // CONFIG
+  // - fPivYLimt  : The threshold at which the pivot action starts
+  //                This threshold is measured in units on the Y-axis
+  //                away from the X-axis (Y=0). A greater value will assign
+  //                more of the joystick's range to pivot actions.
+  //                Allowable range: (0..+1023)
+  float fPivYLimit = 255.0;
+
+  // TEMP VARIABLES
+  float nMotPremixL;  // Motor (left)  premixed output        (-1023..+1023)
+  float nMotPremixR;  // Motor (right) premixed output        (-1023..+1023)
+  int32_t nPivSpeed;  // Pivot Speed                          (-1023..+1023)
+  float fPivScale;    // Balance scale b/w drive and pivot    (   0..1   )
+
+  // Calculate Drive Turn output due to Joystick X input
+  if (nJoyY >= 0) {
+    // Forward
+    nMotPremixL = (nJoyX >= 0) ? 1023.0 : (1023.0 + nJoyX);
+    nMotPremixR = (nJoyX >= 0) ? (1023.0 - nJoyX) : 1023.0;
+  } else {
+    // Reverse
+    nMotPremixL = (nJoyX >= 0) ? (1023.0 - nJoyX) : 1023.0;
+    nMotPremixR = (nJoyX >= 0) ? 1023.0 : (1023.0 + nJoyX);
+  }
+
+  // Scale Drive output due to Joystick Y input (throttle)
+  nMotPremixL = nMotPremixL * nJoyY / 1023.0;
+  nMotPremixR = nMotPremixR * nJoyY / 1023.0;
+
+  // Now calculate pivot amount
+  // - Strength of pivot (nPivSpeed) based on Joystick X input
+  // - Blending of pivot vs drive (fPivScale) based on Joystick Y input
+  nPivSpeed = nJoyX;
+  fPivScale = (abs(nJoyY) > fPivYLimit) ? 0.0 : (1.0 - abs(nJoyY) / fPivYLimit);
+
+  // Calculate final mix of Drive and Pivot
+  nMotMixL = (1.0 - fPivScale) * nMotPremixL + fPivScale * (nPivSpeed);
+  nMotMixR = (1.0 - fPivScale) * nMotPremixR + fPivScale * (-nPivSpeed);
+
+  // return as a struct
+  struct MotorOutputs outputs {
+    nMotMixL, nMotMixR
+  };
+  return outputs;
 }
--- src/RobotStateService.h
+++ src/RobotStateService.h
@@ -8,42 +8,70 @@
 
 #define LED_PIN 2
 
-#define DEFAULT_CAM_Y 90
-#define DEFAULT_CAM_X 135
+#define DEFAULT_CAM_Y 135
+#define DEFAULT_CAM_X 90
 #define DEFAULT_CLAW 90
+#define DEFAULT_DRIVE_X 0
+#define DEFAULT_DRIVE_Y 0
 
 #define CAM_X_SERVO_PIN 2
-#define CAM_Y_SERVO_PIN 16
-#define CLAW_SERVO_PIN 4
+#define CAM_X_SERVO_CHANNEL 10
+
+#define CAM_Y_SERVO_PIN 4
+#define CAM_Y_SERVO_CHANNEL 11
+
+#define CLAW_SERVO_PIN 16
+#define CLAW_SERVO_CHANNEL 12
+
+#define LEFT_MOTOR_PWM_PIN 12
+#define LEFT_MOTOR_DIRECTION_PIN 13
+#define LEFT_MOTOR_PWM_CHANNEL 5
+
+#define RIGHT_MOTOR_PWM_PIN 15
+#define RIGHT_MOTOR_DIRECTION_PIN 14
+#define RIGHT_MOTOR_PWM_CHANNEL 6
+
+#define MOTOR_PWM_RESOLUTION 10
+#define MOTOR_PWM_FREQUENCY 4096
+
+#define DEAD_ZONE 20
 
 #define ROBOT_SETTINGS_ENDPOINT_PATH "/rest/robotState"
 #define ROBOT_SETTINGS_SOCKET_PATH "/ws/robotState"
+
+struct MotorOutputs {
+  int32_t left;
+  int32_t right;
+};
 
 class RobotState {
  public:
   uint8_t camX;
   uint8_t camY;
   uint8_t claw;
+  int32_t driveX;
+  int32_t driveY;
 
   static void read(RobotState& settings, JsonObject& root) {
     root["cam_x"] = settings.camX;
     root["cam_y"] = settings.camY;
     root["claw"] = settings.claw;
+    root["drive_x"] = settings.driveX;
+    root["drive_y"] = settings.driveY;
   }
 
   static StateUpdateResult update(JsonObject& root, RobotState& robotState) {
-    robotState.camX = readBoundedByte(root, "cam_x", 0, 180, DEFAULT_CAM_X);
-    robotState.camY = readBoundedByte(root, "cam_y", 0, 180, DEFAULT_CAM_Y);
-    robotState.claw = readBoundedByte(root, "claw", 90, 180, DEFAULT_CLAW);
+    robotState.camX = readBounded(root, "cam_x", 0, 180, DEFAULT_CAM_X);
+    robotState.camY = readBounded(root, "cam_y", 0, 180, DEFAULT_CAM_Y);
+    robotState.claw = readBounded(root, "claw", 90, 180, DEFAULT_CLAW);
+    robotState.driveX = readBounded(root, "drive_x", -1023, 1023, DEFAULT_DRIVE_X);
+    robotState.driveY = readBounded(root, "drive_y", -1023, 1023, DEFAULT_DRIVE_Y);
     return StateUpdateResult::CHANGED;
   }
 
-  static uint8_t readBoundedByte(JsonObject& root,
-                                 String prop,
-                                 uint8_t minValue,
-                                 uint8_t maxValue,
-                                 uint8_t defaultValue) {
-    uint8_t value = root[prop] | defaultValue;
+  template <typename N>
+  static N readBounded(JsonObject& root, String prop, N minValue, N maxValue, N defaultValue) {
+    N value = root[prop] | defaultValue;
     return max(min(value, maxValue), minValue);
   }
 };
@@ -61,6 +89,8 @@ class RobotStateService : public StatefulService<RobotState> {
   Servo clawServo;
 
   void onConfigUpdated();
+  struct MotorOutputs calculateMotorOutputs(int32_t nJoyX, int32_t nJoyY);
+  void applyMotorOutput(int32_t output, uint8_t directionPin, uint8_t pwmChan);
 };
 
 #endif
