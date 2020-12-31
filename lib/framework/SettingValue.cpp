115d9bebcccad7e92ced048568acfba39cef1664
Rick Watson <rjwats@gmail.com>
2020-12-31 00:37:23 +0000

    remove flash string helpers

--- lib/framework/SettingValue.cpp
+++ lib/framework/SettingValue.cpp
@@ -3,14 +3,13 @@
 namespace SettingValue {
 String format(String value) {
 #ifdef ESP32
-  String chipId((unsigned long)ESP.getEfuseMac(), HEX);
-  String platform(F("esp32"));
+  value.replace("#{chip_id}", String((unsigned long)ESP.getEfuseMac(), HEX));
+  value.replace("#{platform}", "esp32");
 #elif defined(ESP8266)
-  String chipId(ESP.getChipId(), HEX);
-  String platform(F("esp8266"));
+  value.replace("#{chip_id}", String(ESP.getChipId(), HEX));
+  value.replace("#{platform}", "esp8266");
 #endif
-  value.replace(F("#{chip_id}"), chipId);
-  value.replace(F("#{platform}"), platform);
+
   return value;
 }
 };  // end namespace SettingValue
