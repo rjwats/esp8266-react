2ccc8060dbf6f47f6deb7b556e4b844de5aa726e
Rick Watson <rjwats@gmail.com>
2021-11-14 15:32:27 +0000

    adding file committed by mistake

--- interface2/src/api/wifi.ts
+++ interface2/src/api/wifi.ts
@@ -1,9 +1,17 @@
 import { AxiosPromise } from 'axios';
 
-import { WiFiStatus } from '../types';
+import { WiFiNetworkList, WiFiStatus } from '../types';
 
 import { AXIOS } from './endpoints';
 
 export function readWiFiStatus(): AxiosPromise<WiFiStatus> {
   return AXIOS.get('/wifiStatus');
+}
+
+export function scanNetworks(): AxiosPromise<void> {
+  return AXIOS.get('/scanNetworks');
+}
+
+export function listNetworks(): AxiosPromise<WiFiNetworkList> {
+  return AXIOS.get('/listNetworks');
 }
