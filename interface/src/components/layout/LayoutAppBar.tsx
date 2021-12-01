4950b9f4f932b1d5e5e04a062fde2098a081c5fb
Rick Watson <rjwats@gmail.com>
2021-12-01 22:29:42 +0000

    add missing wrapper around auth menu

--- interface/src/components/layout/LayoutAppBar.tsx
+++ interface/src/components/layout/LayoutAppBar.tsx
@@ -1,8 +1,10 @@
 
-import React, { FC } from 'react';
+import { FC, useContext } from 'react';
 
 import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+
+import { FeaturesContext } from '../../contexts/features';
 
 import LayoutAuthMenu from './LayoutAuthMenu';
 
@@ -13,32 +15,36 @@ interface LayoutAppBarProps {
   onToggleDrawer: () => void;
 }
 
-const LayoutAppBar: FC<LayoutAppBarProps> = ({ title,  onToggleDrawer }) => (
-  <AppBar
-    position="fixed"
-    sx={{
-      width: { md: `calc(100% - ${DRAWER_WIDTH}px)` },
-      ml: { md: `${DRAWER_WIDTH}px` },
-      boxShadow: 'none'
-    }}
-  >
-    <Toolbar>
-      <IconButton
-        color="inherit"
-        aria-label="open drawer"
-        edge="start"
-        onClick={onToggleDrawer}
-        sx={{ mr: 2, display: { md: 'none' } }}
-      >
-        <MenuIcon />
-      </IconButton>
-      <Typography variant="h6" noWrap component="div">
-        {title}
-      </Typography>
-      <Box flexGrow={1} />
-      <LayoutAuthMenu />
-    </Toolbar>
-  </AppBar>
-);
+const LayoutAppBar: FC<LayoutAppBarProps> = ({ title, onToggleDrawer }) => {
+  const { features } = useContext(FeaturesContext);
+
+  return (
+    <AppBar
+      position="fixed"
+      sx={{
+        width: { md: `calc(100% - ${DRAWER_WIDTH}px)` },
+        ml: { md: `${DRAWER_WIDTH}px` },
+        boxShadow: 'none'
+      }}
+    >
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={onToggleDrawer}
+          sx={{ mr: 2, display: { md: 'none' } }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" noWrap component="div">
+          {title}
+        </Typography>
+        <Box flexGrow={1} />
+        {features.security && <LayoutAuthMenu />}
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default LayoutAppBar;
