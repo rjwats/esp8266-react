6fa9231ca0e963369a87ae4e6cf29f0d3710eb7f
Rick Watson <rjwats@gmail.com>
2021-12-02 22:22:38 +0000

    revert to including fonts in static public resources

--- interface/src/index.tsx
+++ interface/src/index.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Note: Not importing from @fontsource in order only bundle woff2 fonts
-// This reduces the bundle size by ~40k
-import './fonts/roboto/latin-400.css';
-import './fonts/roboto/latin-500.css';
-
-// Unused by the framework by default. Uncomment if using strong or light typography variants.
-// import './fonts/roboto/latin-300.css';
-// import './fonts/roboto/latin-700.css';
-
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
